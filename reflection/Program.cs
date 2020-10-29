01880c95600ead5aa70288e4cbefe7176ac60c48
anur abubeker osman <abeare21@hotmail.com>
2020-10-29 11:00:18 +0100

    Добавьте файлы проекта.

---
+++ reflection/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            gclass<anur> x = new gclass<anur>();
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+    class anur
+    {
+        public int i { get; set; }
+    }
+
+    class gclass<T> where T : class{
+        public T ttype ;
+        public gclass()
+        {
+            
+        var t= typeof(T).GetMembers();
+            foreach (var item in t)
+            {
+                Console.WriteLine(item.Name); 
+            }
+            Console.WriteLine(t);
+        }
+}
+
+}
